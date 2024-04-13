bd3405b876b656ef6a07ad8a27872bbebaf7dbac
Emmanuel Odenyire Anyira <odenyirechristopher@gmail.com>
2024-04-13 09:38:40 -0700

    Minor corrections

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,35 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table with a given size
- * @size: Size of the hash table
+ * hash_table_create - creates a hash table with a given size
  *
- * Returns: A pointer to the created hash table, or NULL if function fails
+ * @size: size of the hash table
+ * Return: the created hash table, or NULL if function fails
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table; // Declare a pointer to a hash table structure
-        hash_node_t **array; // Declare a double pointer to a hash node structure
-        unsigned long int i; // Declare a variable for iteration
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
 
-        // Allocate memory for the hash table structure
-        table = malloc(sizeof(hash_table_t));
-        if (table == NULL)
-                return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-        // Allocate memory for the array of hash node pointers
-        array = malloc(sizeof(hash_node_t *) * size);
-        if (array == NULL)
-                return (NULL);
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
 
-        // Initialize each element of the array to NULL
-        for (i = 0; i < size; i++)
-                array[i] = NULL;
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-        // Set the attributes of the hash table structure
-        table->array = array;
-        table->size = size;
+	table->array = array;
+	table->size = size;
 
-        return (table); // Return the created hash table
+	return (table);
 }
-
