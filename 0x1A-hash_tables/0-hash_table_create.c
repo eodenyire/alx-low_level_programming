204510c15d322a331ac374f24bd1aab63ed089a5
Emmanuel Odenyire Anyira <odenyirechristopher@gmail.com>
2024-04-13 09:44:11 -0700

    User challenges

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,28 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
-	unsigned long int i;
+    hash_table_t *table;        /* Pointer to the hash table structure */
+    hash_node_t **array;        /* Array of hash node pointers */
+    unsigned long int i;        /* Loop counter */
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
+    /* Allocate memory for the hash table structure */
+    table = malloc(sizeof(hash_table_t));
+    if (table == NULL)
+        return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
-		return (NULL);
+    /* Allocate memory for the array of hash node pointers */
+    array = malloc(sizeof(hash_node_t *) * size);
+    if (array == NULL)
+        return (NULL);
 
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
+    /* Initialize each element of the array to NULL */
+    for (i = 0; i < size; i++)
+        array[i] = NULL;
 
-	table->array = array;
-	table->size = size;
+    /* Assign the array and size to the hash table structure */
+    table->array = array;
+    table->size = size;
 
-	return (table);
+    return (table);
 }
+
