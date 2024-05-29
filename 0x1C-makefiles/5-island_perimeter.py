1e6a08259b3f4c5d4aed53c9c835b9348c2fdc66
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-05-29 10:43:48 -0700

    Finally and finally, task 5 and 6

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in the grid.
+    Args:
+        grid (list of list of int): The grid representing the island and water.
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
