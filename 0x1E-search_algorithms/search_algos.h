5ebb9362168658b6fce2d304a92cdbdddc2b506a
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-17 22:48:23 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
 #include <stddef.h>
 #include "stddef.h"
 #include "stdio.h"
@@ -32,6 +32,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int int_index(int *array, int size, int (*cmp)(int));
 listint_t *jump_list(listint_t *list, size_t size, int value)
-
 
 #endif
