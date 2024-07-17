e669c2b7612fa3341d396630fa91731c55b3fe0f
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-17 22:32:21 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@
 #include <math.h>
 #include <stdlib.h>
 
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int int_index(int *array, int size, int (*cmp)(int));
