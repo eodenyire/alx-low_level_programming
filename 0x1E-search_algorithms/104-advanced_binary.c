bcaa2edf3a822ddf3b4181c0afb88cda1894e64a
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-17 22:12:22 +0300

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,24 +1,37 @@
 #include "search_algos.h"
 
 /**
- * rec_search - searches for a value in a sorted array using binary search.
- * @array: input array.
- * @left: left index of the subarray to search.
- * @right: right index of the subarray to search.
- * @value: value to search for.
- * Return: index of the first occurrence of the value, or -1 if not found.
+ * print_array - prints an array of integers
+ * @array: the array to print
+ * @left: starting index of the array
+ * @right: ending index of the array
  */
-int rec_search(int *array, size_t left, size_t right, int value)
+void print_array(int *array, size_t left, size_t right)
 {
-    size_t mid, i;
-
-    if (left > right)
-        return (-1);
+    size_t i;
 
     printf("Searching in array: ");
     for (i = left; i < right; i++)
         printf("%d, ", array[i]);
     printf("%d\n", array[i]);
+}
+
+/**
+ * rec_search - searches for a value in a sorted array using binary search
+ * @array: input array
+ * @left: left index of the subarray to search
+ * @right: right index of the subarray to search
+ * @value: value to search for
+ * Return: index of the first occurrence of the value, or -1 if not found
+ */
+int rec_search(int *array, size_t left, size_t right, int value)
+{
+    size_t mid;
+
+    if (left > right)
+        return (-1);
+
+    print_array(array, left, right);
 
     mid = left + (right - left) / 2;
 
@@ -32,11 +45,11 @@ int rec_search(int *array, size_t left, size_t right, int value)
 }
 
 /**
- * advanced_binary - searches for a value in a sorted array using binary search.
- * @array: input array.
- * @size: size of the array.
- * @value: value to search for.
- * Return: index of the first occurrence of the value, or -1 if not found.
+ * advanced_binary - searches for a value in a sorted array using binary search
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of the first occurrence of the value, or -1 if not found
  */
 int advanced_binary(int *array, size_t size, int value)
 {
