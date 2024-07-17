2110aba9be39b38678dcd7657778f08caf6961e9
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-17 11:13:15 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,12 @@
 #include "stddef.h"
 #include "stdio.h"
 #include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int int_index(int *array, int size, int (*cmp)(int));
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
