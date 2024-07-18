4babacae4a1567ae72d0072a50053e1b6d27a5cd
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-18 22:26:02 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,27 +1,22 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/* LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+/* STRUCTS */
 /**
- * Linear_skip - Search for a value in a skip list using the linear
- * 		 skip algorithm.
- * @list: A pointer to the head of the skip list to search through.
- * @value: The integer value to search for within the skip list.
+ * struct listint_s - singly linked list
  *
- * Description: 
- * This function performs a search for a specific value in a skip list. 
- * It first traverses the express lane to locate a block where the value
- * might be present. 
- * After identifying a potential range, it performs a linear search 
- * within that block to find the exact node containing the value.
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
  *
- * Return: A pointer to the node containing the value if found, 
- * 	   otherwise NULL.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -30,18 +25,15 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane.
+ * struct skiplist_s - Singly linked list with an express lane
  *
- * @n: Integer value stored in the node.
- * @index: Index of the node in the list.
- * @next: Pointer to the next node in the list.
- * @express: Pointer to the next node in the express lane.
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
  *
- * Description:
- * This structure represents a node in a singly linked list that includes
- * an express lane for faster access to nodes further down the list. 
- * The express lane allows for quicker traversal by skipping over some 
- * nodes.
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
 typedef struct skiplist_s
 {
