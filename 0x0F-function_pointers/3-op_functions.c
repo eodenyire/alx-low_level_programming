acff6c0b0cef805550fa677cb95e59f90cec57ed
Emmanuel Odenyire <61192633+eodenyire@users.noreply.github.com>
2025-01-26 13:09:13 +0300

    Nice one, although lots of issues

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two
+ * @a: num2
+ * @b: num2
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: num1
+ * @b: num2
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two numbers to produce product
+ * @a: num1
+ * @b: num2
+ * Return: the product of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divide and give the quotient of two numbers
+ * @a: num1
+ * @b: num2
+ * Return: the quotient after division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -  function to give the modulus of two numbers
+ * @a: num1
+ * @b: num2
+ * Return: the modulus after division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
